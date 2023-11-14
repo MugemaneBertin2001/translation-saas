6ca160464ca410d9da096944ba0287e2d52e8e4d
Mugemane Bertin <bertin.m2001@gmail.com>
2023-11-14 17:21:14 +0200

    pricing page alignement

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,17 +1,16 @@
 import PageTrunk from '@/components/ui/PageTrunk';
 import Card from '@/components/ui/card';
+import { tiers } from '@/pricingPlanTier';
 import React from 'react';
 
 const Pricing = () => {
 
   return (
     <PageTrunk title='Pricing Plan'>
-        {/* Pricing Card 1 */}
-        <Card plan="Basic Plan" description="Perfect for Personal" price={90} />
-        {/* Pricing Card 2 */}
-        <Card plan="Standard Plan" description="Perfect for small businesses" price={150} />
-        {/* Pricing Card 3 */}
-        <Card plan="Plus plan" description="Perfect for enterprise" price={900} />
+        {tiers.map((tier)=>{
+          return(
+          <Card plan={tier.name} description={tier.description} price={tier.priceMonthly} features={tier.features} />)
+          })}
     </PageTrunk>
   )
 };
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,19 +1,31 @@
+"use client"
 import React from 'react'
 import { Button } from './button'
-
+import { useRouter } from 'next/navigation'
 interface CardProps {
-  plan: string,
+  plan: string | undefined,
   description: string,
-  price: number
+  features:string[],
+  price: string | null
 }
 
-const Card: React.FC<CardProps> = ({plan,description,price})=> {
+const Card: React.FC<CardProps> = ({plan,description,price,features})=> {
+  
+  const Router = useRouter();
   return (
-    <div className="bg-gray-200 p-6 rounded-lg w-75">
+    <div className="bg-gray-200 p-6 rounded-lg w-75 ">
     <h2 className="text-xl font-semibold mb-4 dark:text-black">{plan}</h2>
     <p className="text-gray-600 mb-4">{description}</p>
-    <div className="text-2xl font-bold mb-4 dark:text-black">${price}/Month</div>
-    <Button>
+    <ul className="list-disc ml-6 text-gray-600 mb-4">
+    {features.map((feature) => (
+            <li key={feature}>{feature}</li>
+          ))}
+    </ul>
+    {price &&(
+      <div className="text-2xl font-bold mb-4 dark:text-black">{price}/Month</div>
+    )}
+  
+    <Button onClick={()=>  Router.push('/register')}>
          Select Plan
     </Button>
     </div>
